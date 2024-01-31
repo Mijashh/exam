c91c1682ed7b1515045cd441d8ff19a6e975bf03
Mijash Adhikari <amijash@gmail.com>
2024-01-31 17:45:30 +0545

    fix: add url path for filter content

--- Exam_Details/urls.py
+++ Exam_Details/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("",views.exam_view,name="index"),
     path("<slug:value>/",views.exam_details_view,name="details"),
+    path('filter_exams', views.filter_exams, name='filter_exams')
 ]
